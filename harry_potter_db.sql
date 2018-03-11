cfe38c37b71d94d2a8a687067038f095bc613e1e
laigOSU <35331156+laigOSU@users.noreply.github.com>
2018-03-11 14:55:19 -0700

    Cleaned and updated 03-11-18

--- harry_potter_db.sql
+++ harry_potter_db.sql
@@ -100,4 +100,5 @@ INSERT INTO Enrolled (sid, cid)
 VALUES
 ((SELECT id FROM Students WHERE lname = 'Potter' AND fname = 'Harry'),(SELECT id FROM Classes WHERE name = 'Charms')),
 ((SELECT id FROM Students WHERE lname = 'Weasley' AND fname = 'Ron'),(SELECT id FROM Classes WHERE name = 'Potions')),
-((SELECT id FROM Students WHERE lname = 'Weasley' AND fname = 'Ginny'),(SELECT id FROM Classes WHERE name = 'Transfiguration'));
+((SELECT id FROM Students WHERE lname = 'Weasley' AND fname = 'Ginny'),(SELECT id FROM Classes WHERE name = 'Transfiguration')),
+((SELECT id FROM Students WHERE lname = 'Malfoy' AND fname = 'Draco'),(SELECT id FROM Classes WHERE name = 'Herbology'));
