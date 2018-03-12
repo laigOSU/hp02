8960f652e8698939a23f1571b69b9024607a974c
laigOSU <35331156+laigOSU@users.noreply.github.com>
2018-03-11 22:16:46 -0700

    Update houses.js

--- houses.js
+++ houses.js
@@ -19,11 +19,11 @@ module.exports = function(){
         context.houses = results;
         console.log(context)
         complete();
-
     });
   }
 
   function getHouse(res, mysql, context, req, complete){
+
     mysql.pool.query('SELECT Houses.id AS id, Houses.name, Professors.fname AS head_prof_fname, Professors.lname AS head_prof_lname FROM Houses INNER JOIN Professors ON Professors.house = Houses.id WHERE Houses.id ='+req.params.id,
     function(error, results, fields){
          if(error){
@@ -36,7 +36,7 @@ module.exports = function(){
   }
 
   function getProfessors(res, mysql, context, complete){
-    mysql.pool.query('SELECT Professors.id, Professors.fname, Professors.lname FROM Professors',
+    mysql.pool.query('SELECT Professors.id AS id, Professors.fname, Professors.lname, Houses.name AS name FROM Professors INNER JOIN Houses ON Houses.id = Professors.house',
     function(error, results, fields){
       if(error){
           res.write(JSON.stringify(error));
@@ -88,7 +88,19 @@ module.exports = function(){
   /*****************************************************************************
     INSERT HOUSE
   *****************************************************************************/
-
+  router.post('/', function(req, res){
+    var mysql = req.app.get('mysql');
+    var sql = "INSERT INTO Houses (name, head_prof) VALUES (?,?)";
+    var inserts = [req.body.name, req.body.head_prof];
+    sql = mysql.pool.query(sql,inserts,function(error, results, fields){
+        if(error){
+            res.write(JSON.stringify(error));
+            res.end();
+        }else{
+            res.redirect('/houses');
+        }
+    });
+  });
   /*****************************************************************************
     DELETE HOUSE
   *****************************************************************************/
